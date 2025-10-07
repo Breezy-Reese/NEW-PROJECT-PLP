37ee0d93d97ab623588004cc35f2b9fcf8d7b3a2
Breezy-Reese <basil59mutuku@gmail.com>
2025-10-07 16:45:58 +0300

    project

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'http://localhost:5176', 'http://localhost:5000', 'https://devcollab-carsonn.vercel.app', 'https://devcollab-carsonn.onrender.com'],
+    origin: ['http://localhost:5173', 'http://localhost:5176', 'http://localhost:5000', 'https://new-project-plp.onrender.com', 'https://new-project-plp.onrender.com'],
     methods: ['GET', 'POST'],
     credentials: true,
   },
@@ -21,7 +21,7 @@ const io = new Server(server, {
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5176', 'http://localhost:5000', 'https://devcollab-carsonn.vercel.app', 'https://devcollab-carsonn.onrender.com'],
+  origin: ['http://localhost:5173', 'http://localhost:5176', 'http://localhost:5000', 'https://new-project-plp.onrender.com', 'https://new-project-plp.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
